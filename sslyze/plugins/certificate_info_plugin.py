893145d3e8f92a6f90309ebcc4dd2f811214626e
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2017-05-17 22:18:06 +0200

    Get possible fallback certificates

--- sslyze/plugins/certificate_info_plugin.py
+++ sslyze/plugins/certificate_info_plugin.py
@@ -114,7 +114,7 @@ class CertificateInfoPlugin(plugin_base.Plugin):
         )
         return options
 
-    def get_certificate_info(self, server_info, scan_command, ssl_version=None, cipher_list=None):
+    def get_certificate_info(self, server_info, scan_command, ssl_version=None, cipher_list=None, set_server_name_indication=True):
         # type: (ServerConnectivityInfo, CertificateInfoScanCommand) -> CertificateInfoScanResult
         final_trust_store_list = list(TrustStoresRepository.get_all())
         if scan_command.custom_ca_file:
@@ -128,7 +128,7 @@ class CertificateInfoPlugin(plugin_base.Plugin):
         thread_pool = ThreadPool()
         for trust_store in final_trust_store_list:
             # Try to connect with each trust store
-            thread_pool.add_job((self._get_and_verify_certificate_chain, (server_info, trust_store, ssl_version, cipher_list)))
+            thread_pool.add_job((self._get_and_verify_certificate_chain, (server_info, trust_store, ssl_version, cipher_list, set_server_name_indication)))
 
         # Start processing the jobs; one thread per trust
         thread_pool.start(len(final_trust_store_list))
@@ -140,7 +140,7 @@ class CertificateInfoPlugin(plugin_base.Plugin):
         ocsp_response = None
 
         for (job, result) in thread_pool.get_result():
-            (_, (_, trust_store, _, _)) = job
+            (_, (_, trust_store, _, _, _)) = job
             certificate_chain, validation_result, ocsp_response = result
             # Store the returned verify string for each trust store
             path_validation_result_list.append(PathValidationResult(trust_store, validation_result))
@@ -148,7 +148,7 @@ class CertificateInfoPlugin(plugin_base.Plugin):
         # Store thread pool errors
         last_exception = None
         for (job, exception) in thread_pool.get_error():
-            (_, (_, trust_store, _, _)) = job
+            (_, (_, trust_store, _, _, _)) = job
             path_validation_error_list.append(PathValidationError(trust_store, exception))
             last_exception = exception
 
@@ -190,16 +190,21 @@ class CertificateInfoPlugin(plugin_base.Plugin):
         except SSLHandshakeRejected:
             pass
 
+        for ssl_version in server_info.ssl_versions_supported:
+            cert_info = self.get_certificate_info(server_info, scan_command, ssl_version=ssl_version, set_server_name_indication=False)
+            if isinstance(cert_info, CertificateInfo):
+                certificate_infos.add(cert_info)
+
         return CertificateInfoScanResult(server_info, scan_command, list(certificate_infos))
 
 
     @staticmethod
-    def _get_and_verify_certificate_chain(server_info, trust_store, ssl_version, cipher_list):
+    def _get_and_verify_certificate_chain(server_info, trust_store, ssl_version, cipher_list, set_server_name_indication):
         # type: (ServerConnectivityInfo, TrustStore) -> Tuple[List[cryptography.x509.Certificate], Text, Optional[OcspResponse]]
         """Connects to the target server and uses the supplied trust store to validate the server's certificate.
         Returns the server's certificate and OCSP response.
         """
-        ssl_connection = server_info.get_preconfigured_ssl_connection(override_ssl_version=ssl_version, ssl_verify_locations=trust_store.path, override_cipher_list=cipher_list)
+        ssl_connection = server_info.get_preconfigured_ssl_connection(override_ssl_version=ssl_version, ssl_verify_locations=trust_store.path, override_cipher_list=cipher_list, set_server_name_indication=set_server_name_indication)
 
         # Enable OCSP stapling
         ssl_connection.set_tlsext_status_ocsp()
