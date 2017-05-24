c282870f302f11e2c84639ac4261e6d38f7b55b5
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2017-05-24 15:06:18 +0200

    Fix dead lock in thread handling

--- sslyze/utils/thread_pool.py
+++ sslyze/utils/thread_pool.py
@@ -10,14 +10,12 @@ try:
     # Python 3
     # noinspection PyCompatibility
     from queue import Queue
+    from queue import Empty as Empty
 except ImportError:
     # Python 2
     # noinspection PyCompatibility
     from Queue import Queue
-
-
-class _ThreadPoolSentinel(object):
-    pass
+    from Queue import Empty as Empty
 
 
 class ThreadPool(object):
@@ -27,86 +25,67 @@ class ThreadPool(object):
     Anything else goes to the result queue that can be read using get_result().
     """
     def __init__(self):
-        self._active_threads = 0
+        self._active_jobs = 0
         self._job_q = Queue()
         self._result_q = Queue()
-        self._error_q = Queue()
         self._thread_list = []
 
     def add_job(self, job):
         self._job_q.put(job)
 
     def get_error(self):
-        active_threads = self._active_threads
-        while active_threads or (not self._error_q.empty()):
-            error = self._error_q.get()
-            if isinstance(error, _ThreadPoolSentinel):
-                # One thread was done
-                active_threads -= 1
-                self._error_q.task_done()
-                continue
-
-            else:
-                # Getting an actual error
-                self._error_q.task_done()
-                yield error
-
+        return self._get_result_by_type(error=True)
 
     def get_result(self):
-        active_threads = self._active_threads
-        while active_threads or (not self._result_q.empty()):
+        return self._get_result_by_type(error=False)
+
+    def _get_result_by_type(self, error):
+        remaining_results = []
+        while self._active_jobs:
             result = self._result_q.get()
-            if isinstance(result, _ThreadPoolSentinel):
-                # One thread was done
-                active_threads -= 1
-                self._result_q.task_done()
-                continue
+            self._result_q.task_done()
+            self._active_jobs -= 1
 
-            else:
-                # Getting an actual result
-                self._result_q.task_done()
+            if error == isinstance(result[1], Exception):
                 yield result
+            else:
+                remaining_results.append(result)
 
+        for result in remaining_results:
+            self._result_q.put(result)
+            self._active_jobs += 1
 
     def start(self, nb_threads):
         """
         Should only be called once all the jobs have been added using add_job().
         """
-        if self._active_threads:
+        if self._active_jobs != 0:
             raise Exception('Threads already started.')
+
+        self._active_jobs = self._job_q.qsize()
 
         # Create thread pool
         for _ in range(nb_threads):
             worker = threading.Thread(
                 target=_work_function,
-                args=(self._job_q, self._result_q, self._error_q))
+                args=(self._job_q, self._result_q))
             worker.start()
             self._thread_list.append(worker)
-            self._active_threads += 1
-
-        # Put sentinels to let the threads know when there's no more jobs
-        [self._job_q.put(_ThreadPoolSentinel()) for _ in self._thread_list]
-
 
     def join(self):
         # Clean exit
         self._job_q.join()
         [worker.join() for worker in self._thread_list]
-        self._active_threads = 0
         self._result_q.join()
-        self._error_q.join()
+        self._active_jobs = 0
 
 
-def _work_function(job_q, result_q, error_q):
+def _work_function(job_q, result_q):
     """Work function expected to run within threads."""
     while True:
-        job = job_q.get()
-
-        if isinstance(job, _ThreadPoolSentinel):
-            # All the work is done, get out
-            result_q.put(_ThreadPoolSentinel())
-            error_q.put(_ThreadPoolSentinel())
-            job_q.task_done()
+        try:
+            job = job_q.get(False)
+        except Empty:
             break
 
         function = job[0]
@@ -114,7 +93,7 @@ def _work_function(job_q, result_q, error_q):
         try:
             result = function(*args)
         except Exception as e:
-            error_q.put((job, e))
+            result_q.put((job, e))
         else:
             result_q.put((job, result))
         finally:
